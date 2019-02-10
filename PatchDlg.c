fa753b84da4e0ee7516e93662bb435d62a192583
MathOnNapkins <MathOnNapkins@gmail.com>
2019-02-10 06:18:08 -0500

    - Sigh
    - So it appears the best approach now would be to find matching double quotes in the Spec list, grab the innards of each double quoted pair (separated by spaces), also find the max length among them, allocate a buffer large enough to hold both the folder path and the longest filename, and iteratively try to use LB_FINDSTRING in the patch listbox. If any files are already present, they're ignored. Whether or not we tell the user about this, who knows. Also might be nice to have a way to move the files up or down in terms of priority of evaluation, though I don't remember off hand whether this even matters for FSNASM. But it might matter in the way Hyrule Magic handles the output... who knows. Still flumoxed at what a pain this whole operation has been for a feature probably no one will ever use.

--- PatchDlg.c
+++ PatchDlg.c
@@ -146,6 +146,9 @@
     {
         char * m_buffer;
         
+        // Number of actual characters used in the buffer
+        size_t m_actual_length;
+        
         size_t m_buffer_length;
 
         DWORD m_error;
@@ -177,6 +180,8 @@
             sizeof(char)
         );
         
+        p_property->m_actual_length = 0;
+        
         p_property->m_buffer_length = p_buffer_length;
         
         p_property->m_error = 0;
@@ -201,7 +206,7 @@
         if( IsNonNull(data) )
         {
             HM_FileDlgProperty_New(&data->m_folder, MAX_PATH);
-            HM_FileDlgProperty_New(&data->m_spec, 0x1000);
+            HM_FileDlgProperty_New(&data->m_spec, 0x100);
         }
         
         return data;
@@ -239,18 +244,41 @@
 
 // =============================================================================
 
-    extern HM_FileDlg_Result
+    extern int
+    HM_ModularRoundUp
+    (
+        unsigned const p_value,
+        unsigned const p_modulus
+    )
+    {
+        unsigned residue = 0;
+        
+        // -----------------------------
+        
+        if( Is(p_modulus, 0) )
+        {
+            return 0;
+        }
+        
+        residue = ( (p_modulus - p_value) % (p_modulus) );
+        
+        return (p_value + residue);
+    }
+
+// =============================================================================
+
+    extern int
     HM_FileDlg_GetSpec
     (
         HWND                    const p_file_dlg,
         CP2(HM_FileDlgProperty)       p_property
     )
     {
-        HM_FileDlg_Result r = { 0 };
+        HWND const parent_win = GetParent(p_file_dlg);
         
-        int const spec_length = SendMessage
+        int spec_length = SendMessage
         (
-            GetParent(p_file_dlg),
+            parent_win,
             CDM_GETSPEC,
             p_property->m_buffer_length,
             (LPARAM) p_property->m_buffer
@@ -261,27 +289,79 @@
         if(spec_length < 0)
         {
             p_property->m_error = CommDlgExtendedError();
-            r.m_length = 0;
         }
         else
         {
-            r.m_length = spec_length;
+            DWORD spec_length_dword = spec_length;
+            
+            // -----------------------------
+            
+            if(spec_length_dword <= p_property->m_buffer_length)
+            {
+                // We got the whole thing in one call, no need to reallocate.
+
+                p_property->m_actual_length = spec_length;
+                
+                p_property->m_error = 0;
+            }
+            else
+            {
+                size_t const new_count = HM_ModularRoundUp
+                (
+                    spec_length,
+                    0x100
+                );
+
+                // -----------------------------
+                
+                p_property->m_buffer = (char*) recalloc
+                (
+                    p_property->m_buffer,
+                    new_count,
+                    p_property->m_buffer_length,
+                    sizeof(char)
+                );
+                
+                if(p_property->m_buffer)
+                {
+                    p_property->m_buffer_length = new_count;
+                    
+                    spec_length = SendMessage
+                    (
+                        parent_win,
+                        CDM_GETSPEC,
+                        p_property->m_buffer_length,
+                        (LPARAM) p_property->m_buffer
+                    );
+                    
+                    if(spec_length < 0)
+                    {
+                        p_property->m_error = CommDlgExtendedError();
+                    }
+                    else
+                    {
+                        p_property->m_actual_length = spec_length;
+                    }
+                }
+                else
+                {
+                    p_property->m_buffer_length = 0;
+                }
+            }
         }
         
-        return r;
+        return spec_length;
     }
 
 // =============================================================================
 
-    HM_FileDlg_Result
+    extern int
     HM_FileDlg_GetFolder
     (
         HWND                    const p_file_dlg,
         CP2(HM_FileDlgProperty)       p_property
     )
     {
-        HM_FileDlg_Result r = { 0 };
-        
         int const folder_length = SendMessage
         (
             GetParent(p_file_dlg),
@@ -295,14 +375,79 @@
         if(folder_length < 0)
         {
             p_property->m_error = CommDlgExtendedError();
-            r.m_length = 0;
+            
+            p_property->m_actual_length = 0;
         }
         else
         {
-            r.m_length = folder_length;
+            DWORD const folder_length_dword = folder_length;
+            
+            // -----------------------------
+            
+            if(folder_length_dword <= p_property->m_buffer_length)
+            {
+                p_property->m_actual_length = folder_length;
+            }
+            else
+            {
+                p_property->m_actual_length = 0;
+            }
         }
         
-        return r;
+        return folder_length;
+    }
+
+// =============================================================================
+
+    extern int
+    HM_FileDlg_GetFolderPlus
+    (
+        HWND                    const p_file_dlg,
+        CP2(HM_FileDlgProperty)       p_property
+    )
+    {
+        int folder_length = HM_FileDlg_GetFolder(p_file_dlg, p_property);
+        
+        DWORD const folder_length_dword = folder_length;
+        
+        // -----------------------------
+        
+        if
+        (
+            (folder_length >= 0)
+         && IsNot(p_property->m_actual_length, folder_length_dword)
+         && IsNonZero(p_property->m_error)
+        )
+        {
+            size_t const new_count = HM_ModularRoundUp
+            (
+                folder_length,
+                0x100
+            );
+
+            // -----------------------------
+                
+            p_property->m_buffer = (char*) recalloc
+            (
+                p_property->m_buffer,
+                p_property->m_buffer_length,
+                new_count,
+                sizeof(char)
+            );
+            
+            if(p_property->m_buffer)
+            {
+                p_property->m_buffer_length = new_count;
+                
+                folder_length = HM_FileDlg_GetFolder(p_file_dlg, p_property);
+            }
+            else
+            {
+                p_property->m_buffer_length = 0;
+            }
+        }
+        
+        return folder_length;
     }
 
 // =============================================================================
@@ -320,7 +465,6 @@
         return data;
     }
 
-
 // =============================================================================
 
     static size_t
@@ -332,87 +476,30 @@
     {
         CP2(HM_FileDlgData) data = HM_MultiSelectFileDlgHook_GetData(p_ofn);
         
-        HM_FileDlg_Result folder_r = HM_FileDlg_GetFolder
+        int const folder_length = HM_FileDlg_GetFolder
         (
             p_file_dlg,
             &data->m_folder
         );
         
-        HM_FileDlg_Result spec_r = HM_FileDlg_GetSpec
+        int const spec_length = HM_FileDlg_GetSpec
         (
             p_file_dlg,
             &data->m_spec
         );
         
-        DWORD const total_length = (folder_r.m_length + spec_r.m_length);
+        DWORD const total_length =
+        (
+            data->m_folder.m_actual_length
+          + data->m_spec.m_actual_length
+        );
         
         // -----------------------------
         
-        if(folder_r.m_length > data->m_folder.m_buffer_length)
+        if( (spec_length < 0) || (folder_length < 0) )
         {
-            data->m_folder.m_buffer = (char*) recalloc
-            (
-                data->m_folder.m_buffer,
-                folder_r.m_length,
-                data->m_folder.m_buffer_length,
-                sizeof(char)
-            );
-            
-            if(data->m_folder.m_buffer)
-            {
-                data->m_folder.m_buffer_length = folder_r.m_length;
-            }
-            else
-            {
-                goto error;
-            }
-            
-            folder_r = HM_FileDlg_GetFolder
-            (
-                p_file_dlg,
-                &data->m_folder
-            );
-            
-            if(folder_r.m_error)
-            {
-                data->m_folder.m_error = folder_r.m_error;
-                
-                goto error;
-            }
+            goto error;
         }
-        
-        if(spec_r.m_length > data->m_spec.m_buffer_length)
-        {
-            data->m_spec.m_buffer = (char*) recalloc
-            (
-                data->m_spec.m_buffer,
-                spec_r.m_length,
-                data->m_spec.m_buffer_length,
-                sizeof(char)
-            );
-            
-            if(data->m_spec.m_buffer)
-            {
-                data->m_spec.m_buffer_length = spec_r.m_length;
-            }
-            else
-            {
-                goto error;
-            }
-            
-            spec_r = HM_FileDlg_GetSpec
-            (
-                p_file_dlg,
-                &data->m_spec
-            );
-            
-            if(spec_r.m_error)
-            {
-                data->m_spec.m_error = spec_r.m_error;
-                
-                goto error;
-            }
-        }        
         
         if(total_length > p_ofn->nMaxFile)
         {
@@ -445,42 +532,19 @@
         CP2(OPENFILENAME)       p_ofn
     )
     {
-        CP2(HM_FileDlgData) data = HM_MultiSelectFileDlgHook_GetData(p_ofn);
+        // \task[low] We used to do something here, but it needs to be
+        // re-thought out. There is a bit of a nuance wherein if the user
+        // deselects all files in the dialog, it still returns at least one
+        // file. I wanted to finesse this a bit and check if the "spec"
+        // buffer returned two consecutive times is identical, to make
+        // it considered to be zero files selected when the OK button is hit.
+        // However, I have a feeling that the system will fight us tooth and
+        // nail on this.
+        UNREFERENCED_PARAMETER(p_file_dlg);
+        UNREFERENCED_PARAMETER(p_ofn);
         
-        HM_FileDlg_Result const spec_r = HM_FileDlg_GetSpec
-        (
-            p_file_dlg,
-            &data->m_spec
-        );
-        
-        HM_FileDlg_Result const folder_r = HM_FileDlg_GetFolder
-        (
-            p_file_dlg,
-            &data->m_folder
-        );
-        
-        DWORD const total_length = (spec_r.m_length + folder_r.m_length);
-        
-        // -----------------------------
-        
-        if(total_length > p_ofn->nMaxFile)
-        {
-            size_t const old_size = p_ofn->nMaxFile;
-            
-            // -----------------------------
-            
-            p_ofn->nMaxFile = (total_length + MAX_PATH);
-            
-            p_ofn->lpstrFile = (LPTSTR) recalloc
-            (
-                p_ofn->lpstrFile,
-                p_ofn->nMaxFile,
-                old_size,
-                sizeof(char)
-            );
-        }
-        
-        return p_ofn->nMaxFile;
+        return 0;
+
     }
 
 // =============================================================================
@@ -696,9 +760,55 @@ wchar_t const patch_filter_wide[] = MACRO_StringifyW(patch_filter_pre);
     {
         DWORD i = 0;
 
+        size_t file_count = 0;
+        
+        size_t name_length = 0;
+        
+        CP2C(HM_FileDlgProperty) spec = &p_data->m_spec;
+        
         // -----------------------------
         
-        for(i = 0; i < p_data->m_folder.m_buffer_length; i += 1)
+        for(i = 0; i < spec->m_actual_length; i += 1)
+        {
+            if( IsNot(spec->m_buffer[i], '\0') )
+            {
+                name_length += 1;
+            }
+            else
+            {
+                if( Is(name_length, 0) )
+                {
+                    // Found the double null terminator
+                    break;
+                }
+                
+                file_count += 1;
+                
+                name_length = 0;    
+                
+                continue;
+            }
+        }
+        
+        return 0;
+    }
+
+// =============================================================================
+
+    extern int
+    HM_FileDlg_AddFiles
+    (
+        HWND                 const p_listbox,
+        CP2C(HM_FileDlgData)       p_data
+    )
+    {
+        size_t const file_count = HM_FileDlg_CountFiles(p_data);
+        
+        DWORD i = 0;
+
+        // -----------------------------
+        
+        for(i = 0; i < p_data->m_spec.m_buffer_length; i += 1)
         {
             ;
         }
@@ -765,7 +875,11 @@ wchar_t const patch_filter_wide[] = MACRO_StringifyW(patch_filter_pre);
             goto cleanup;
         }
         
-        HM_FileDlg_CountFiles(data);
+        HM_FileDlg_AddFiles
+        (
+            listbox,
+            data
+        );
         
         for(i = 0; i < num_files; i += 1)
         {
